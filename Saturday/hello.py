bd78ac41ab460d40a2152a487c17546b9ba9ed85
L Chen <jenac@users.noreply.github.com>
2021-01-30 09:17:13 -0800

    Create hello.py

---
+++ Saturday/hello.py
@@ -0,0 +1,37 @@
+a = 1
+b = 2
+c = a+b
+
+"""
+function max
+input a, b
+return bigger number
+"""
+'''
+function 
+
+'''
+# john is variable name
+# "hello" is a value and it is a string
+john = "hello"
+
+# message is variable name
+# "big deal ... target" is the value
+message = "big deal on target"
+
+print(message)
+message = "new message"
+print(message)
+
+message = 100
+print(message)
+
+
+def max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+print('hello, world')
+
