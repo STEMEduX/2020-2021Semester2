0f8d874afd36c50e50f252346f066389aa5bd032
Lihe Chen <lihe.chen@gmail.com>
2021-02-27 07:15:51 -0600

    20200227
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ Saturday/20210227/1_bool.py
@@ -0,0 +1,28 @@
+# Boolean Values
+## True / False
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+## in if
+
+
+# Evaluate Values and Variables
+## bool("helo")
+## bool(18)
+## bool(["apple", "cherry", "banana"])
+# bool(False)
+# bool(None)
+# bool(0)
+# bool("")
+# bool(())
+# bool([])
+# bool({})
+
+# 
+
+# Functions can Return a Boolean
+
+# x = 200
+# print(isinstance(x, int))
