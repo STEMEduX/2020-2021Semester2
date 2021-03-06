f2e21b8aa79d6cd8745164f4244fcdc1ced363c8
Lihe Chen <lihe.chen@gmail.com>
2021-03-06 09:18:21 -0600

    python lists
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

---
+++ Saturday/20210306/1_lists.py
@@ -0,0 +1,50 @@
+# Python Lists
+
+
+# Lists are used to store multiple items in a single variable.
+# collections : list, set, tuple, dictionary
+
+
+# thislist = ["apple", "banana", "cherry"]
+# print(thislist)
+
+
+# index access
+
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
+
+
+# Changeable/Mutable
+# The list is changeable, meaning that we can change, add, and remove items 
+# in a list after it has been created.
+
+
+
+# Allow Duplicates
+# Since lists are indexed, lists can have items with the same value:
+
+
+# List Length
+# To determine how many items a list has, use the len() function:
+
+
+# List Items - Data Types
+# List items can be of any data type, can mix different data types
+
+
+# type()
+
+
+# The list() Constructor
+# list("asdfasdf")
