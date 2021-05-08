19d7e116a0d5721487eae02e81ac64ef9348b5f6
Lihe Chen <lihe.chen@gmail.com>
2021-05-08 09:47:51 -0500

    added missing part
    Signed-off-by: Lihe Chen <lihe.chen@gmail.com>

--- Saturday/20210417/7_tuple.py
+++ Saturday/20210417/7_tuple.py
@@ -8,3 +8,15 @@ if "bananaA" in fruits:
     print(fruits.index("bananaA"))
 print("do some job")
 ## https://www.w3schools.com/python/python_tuples_exercises.asp
+
+# What is the difference between fruits={"a","b"} and fruits=("a","b")
+print('------------')
+#initalizer, declare a variable and assign value to it.
+fruits={"a","b"} # set, initialize a set
+print(type(fruits))
+fruits=("a","b") # tuple
+print(type(fruits))
+fruits=["a","b"] # list
+print(type(fruits))
+
+
